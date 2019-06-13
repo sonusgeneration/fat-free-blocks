e62c451634901d0bcda1a0f4eb2bbee30b0c0759
Christopher Rains <crains@sonusgeneration.com>
2019-06-13 14:14:24 -0400

    (Update):
    - blocks/installation/composerscripts.php
      Create "Controllers" folder at app root on "post-install".

--- Blocks/Installation/ComposerScripts.php
+++ Blocks/Installation/ComposerScripts.php
@@ -8,7 +8,11 @@ class ComposerScripts {
     public static function postInstall(Event $event) {
         require_once($event->getComposer()->getConfig()->get('vendor-dir').'/autoload.php');
 
-        echo "Ready.";
+        define('APP_PATH', __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".."
+            . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".."
+            . DIRECTORY_SEPARATOR);
+
+        Nette\Utils\FileSystem::createDir(APP_PATH . "Controllers", 0755);
     }
 
 }

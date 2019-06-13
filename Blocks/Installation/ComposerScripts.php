12902498acba1c3ad39e3e63c98eefb13c5a9bc1
Christopher Rains <crains@sonusgeneration.com>
2019-06-13 14:20:09 -0400

    (Update):
    - blocks/installation/composerscripts.php
      Update create directory statement.

--- Blocks/Installation/ComposerScripts.php
+++ Blocks/Installation/ComposerScripts.php
@@ -7,12 +7,7 @@ class ComposerScripts {
 
     public static function postInstall(Event $event) {
         require_once($event->getComposer()->getConfig()->get('vendor-dir').'/autoload.php');
-
-        define('APP_PATH', __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".."
-            . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".."
-            . DIRECTORY_SEPARATOR);
-
-        Nette\Utils\FileSystem::createDir(APP_PATH . "Controllers", 0755);
+        Nette\Utils\FileSystem::createDir(__DIR__ . DIRECTORY_SEPARATOR . "Controllers", 0755);
     }
 
 }

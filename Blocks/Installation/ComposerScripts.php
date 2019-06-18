f4b808f947162186f76525bf735a94f43c7418b4
Christopher Rains <crains@sonusgeneration.com>
2019-06-18 12:47:21 -0400

    (Update):
    - blocks/installation/composerscripts.php
      Remove "Nette" filesystem commands.
    - composer.json
      Removed symfony/mbstring requirement
      Removed symfony/php-72 requirement

--- Blocks/Installation/ComposerScripts.php
+++ Blocks/Installation/ComposerScripts.php
@@ -5,9 +5,11 @@ use \Composer\Script\Event;
 
 class ComposerScripts {
 
+    const APP_PATH = __DIR__ . "/../../../../../";
+    const PACKAGE_PATH = __DIR__ . "/../../";
+
     public static function postInstall(Event $event) {
         require_once($event->getComposer()->getConfig()->get('vendor-dir').'/autoload.php');
-        Nette\Utils\FileSystem::createDir(__DIR__ . DIRECTORY_SEPARATOR . "Controllers", 0755);
     }
 
 }
